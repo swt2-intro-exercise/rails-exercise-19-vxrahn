3d0e1b924490d1a3e4207210f34ce4461283768a
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 04:12:17 +0100

    Failing tests for #17

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Show paper page", type: :feature do
+
+  before(:each) do
+    @alan = FactoryBot.create :author
+    @computing = FactoryBot.create :paper
+  end
+
+  it "should render withour error" do
+    visit paper_path(@computing)
+  end
+
+  it "should show author full name" do
+    visit paper_path(@computing)
+
+    expect(page).to have_text(@alan.name)
+  end
+
+end
