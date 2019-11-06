227d6f0f2bab97e22f4674c8ba6849492306b8e8
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 03:07:41 +0100

    Failing tests for #10

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+
+  before(:each) do
+    @alan = FactoryBot.create :author
+  end
+
+  it "should render withour error" do
+    visit edit_author_path(@alan)
+  end
+
+  it "should edit author" do
+    visit edit_author_path(@alan)
+
+    page.fill_in 'author[first_name]', with: @alan.first_name
+    page.fill_in 'author[last_name]', with: @alan.first_name
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.de/Edit'
+
+    find('input[type="submit"]').click
+
+    @alan.reload
+
+    expect(@alan.homepage).to be 'http://wikipedia.de/Edit'
+  end
+
+end
