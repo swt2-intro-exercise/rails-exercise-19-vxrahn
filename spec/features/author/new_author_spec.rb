eb7b8743adfd546d29b787d9abe89210a0a430c6
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 02:05:51 +0100

    Failing test for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -14,4 +14,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should save author" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Edsger'
+    page.fill_in 'author[last_name]', with: 'Dijkstra'
+    page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+
+    find('input[type="submit"]').click
+  end
 end
