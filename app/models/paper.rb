94d0f9d4a93f38846a01ee467726ebfa6481a1e4
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 05:12:20 +0100

    Resolve #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,7 @@ class Paper < ApplicationRecord
   validates :year, numericality: { only_integer: true }
 
   has_and_belongs_to_many :authors
+
+  scope :written_in, ->(year) { where("year = ?", year) if year.present? }
+
 end
