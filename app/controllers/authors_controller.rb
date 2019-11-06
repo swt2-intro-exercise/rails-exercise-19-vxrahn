f278ce010a0389ea93467c1c93cc4e8e8c039c43
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 02:32:01 +0100

    Resolve #6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,10 @@
 class AuthorsController < ApplicationController
+
   def new
+  end
+
+  def show
+    @author = Author.find(params[:id])
   end
 
   def create
--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 describe "Show author page", type: :feature do
 
+  before(:each) do
+    @alan = FactoryBot.create :author
+  end
+
   it "should render withour error" do
     visit author_path(@alan)
+  end
 
-    expect(page).to have_text('Alan Turing')
+  it "should show author info" do
+    visit author_path(@alan)
+
+    expect(page).to have_text(@alan.name)
+    expect(page).to have_text(@alan.homepage)
   end
 
 end
