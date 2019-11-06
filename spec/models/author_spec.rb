8006fb0bfccf99e1768a6b58aa6906763b41f353
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 02:48:45 +0100

    Failing test for #8

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -16,4 +16,11 @@ describe Author, type: :model do
 
     expect(author.name).to eq('Alan Turing')
   end
+
+
+  it "should not validate without last name" do
+    author = Author.new( :first_name => 'Alan', :homepage => 'http://wikipedia.org/Alan_Turing')
+
+    expect(author).to_not be_valid
+  end
 end
