70ee908ea135e169be8fcce0fbbda24e57f81de6
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-06 03:52:18 +0100

    Failing tests for #14

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,23 +8,18 @@ describe Author, type: :model do
 
   it "should have first name, last name, and homepage" do
     author = Author.new( :first_name => first_name, :last_name => last_name, :homepage => homepage)
-
     expect(author.first_name).to eq(first_name)
     expect(author.last_name).to eq(last_name)
     expect(author.homepage).to eq(homepage)
-
   end
 
   it "should return name containing first name and last name" do
     author = Author.new( :first_name => first_name, :last_name => last_name, :homepage => homepage)
-
     expect(author.name).to eq(first_name + ' ' + last_name)
   end
 
-
   it "should not validate without last name" do
     author = Author.new( :first_name => first_name, :homepage => homepage)
-
     expect(author).to_not be_valid
   end
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
+title = "Title"
+venue = "mind 49: 433-460"
+year = 1950
+
 describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have title, venue, and year" do
+    paper = Paper.new( :title => title, :venue => venue, :year => year)
+    expect(paper.title).to eq(title)
+    expect(paper.venue).to eq(venue)
+    expect(paper.year).to eq(year)
+  end
+
+  it "should not validate without title" do
+    paper = Paper.new( :venue => venue, :year => year)
+    expect(paper).to_not be_valid
+  end
+
+  it "should only validate with numerical year" do
+    paper = Paper.new( :title => title, :venue => venue, :year => title)
+    expect(paper).to_not be_valid
+  end
 end
